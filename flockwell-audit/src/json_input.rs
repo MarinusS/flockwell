f21c1c259125dec2d006f18319f1405bac8ce493
MarinusS <78360477+MarinusS@users.noreply.github.com>
2026-09-17 14:41:29 +0200

    Set imported animal fields after construction

--- flockwell-audit/src/json_input.rs
+++ flockwell-audit/src/json_input.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::path::Path;
 
-use flockwell_domain::{Animal, AnimalId, Sex};
+use flockwell_domain::{Animal, AnimalId};
 use std::str::FromStr;
 
 #[derive(serde::Deserialize)]
@@ -18,17 +18,9 @@ pub(crate) fn load_animals(path: &Path) -> Result<Vec<Animal>, Box<dyn Error>> {
         .into_iter()
         .map(|input| {
             let id = AnimalId::from_str(&input.id)?;
-            Ok(Animal {
-                id,
-                tag: input.tag,
-                tip_tag: None,
-                uhf_tag: None,
-                uhf_tag_visual: None,
-                sex: Sex::Unknown,
-                life_stage_override: None,
-                lambing_id: None,
-                disposition_id: None,
-            })
+            let mut animal = Animal::new(id);
+            animal.tag = input.tag;
+            Ok(animal)
         })
         .collect::<Result<Vec<_>, Box<dyn Error>>>()?;
 
